7bba9b486c6f0d9ed8c4ea0e6ec6d09ae119c3b3
SatyamVyas04 <vyas.satyam.04@gmail.com>
2025-02-27 13:17:30 +0530

    Add section3 images and enhance Section2 and Section3 layouts for improved design

--- src/pages/section2.tsx
+++ src/pages/section2.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Section2() {
 	return (
-		<section className="mx-auto flex max-w-md flex-col items-center justify-between space-y-4 py-8 sm:py-10 md:w-full md:max-w-none md:flex-row md:items-stretch md:space-y-8">
+		<section className="mx-auto my-4 flex max-w-md flex-col items-center justify-between space-y-4 py-8 sm:py-10 md:w-full md:max-w-none md:flex-row md:items-stretch md:space-y-8">
 			<div>
 				<Image
 					src="/page/section2.jpeg"
@@ -13,12 +13,12 @@ export default function Section2() {
 				/>
 			</div>
 			<div className="items-between flex flex-col justify-between space-y-6 py-4 text-center md:text-end">
-				<h1 className="text-3xl font-bold uppercase md:text-5xl lg:text-7xl">
+				<h1 className="text-3xl font-bold uppercase md:-mt-8 md:text-5xl lg:text-7xl">
 					Forget Outdated Textbooks
 				</h1>
 				<div className="md:ml-auto">
-					<hr className="my-2 w-full max-w-sm border-2 border-foreground md:ml-auto" />
-					<p className="w-full max-w-sm text-sm md:ml-auto md:text-base">
+					<hr className="mx-auto my-2 w-full max-w-80 border-2 border-foreground md:ml-auto md:max-w-sm" />
+					<p className="relative z-30 w-full max-w-sm px-8 text-sm md:ml-auto md:px-0 md:text-base">
 						<span className="text-lg font-bold">BFM Academy</span> is where
 						education meets innovation. Earn crypto, own your knowledge with NFT
 						credentials and be part of the next-gen learning revolution.
--- src/pages/section3.tsx
+++ src/pages/section3.tsx
@@ -1,3 +1,37 @@
+import Image from 'next/image'
+
 export default function Section3() {
-	return <section>Section3</section>
+	return (
+		<section className="relative z-0 my-4 md:-mt-48">
+			<Image
+				src="/page/section3-bg.png"
+				width={2400}
+				height={2400}
+				alt="Vector png"
+				className="relative z-10 ml-auto hidden max-w-4xl rounded-2xl md:translate-y-44 dark:md:block"
+			/>
+			<Image
+				src="/page/section3-bg-light.png"
+				width={2400}
+				height={2400}
+				alt="Vector png"
+				className="relative z-10 ml-auto hidden max-w-4xl rounded-2xl md:block md:translate-y-44 dark:md:hidden"
+			/>
+			<div className="relative my-4 flex h-96 w-full flex-col rounded-lg bg-foreground p-2 px-6 dark:bg-brandblue md:items-start md:justify-end md:rounded-xl">
+				<h1 className="text-5xl font-bold uppercase text-background dark:text-foreground md:text-7xl lg:text-9xl">
+					JOIN THE
+				</h1>
+				<Image
+					src="/page/section3.png"
+					width={2400}
+					height={2400}
+					alt="A person wearing vr glasses"
+					className="absolute bottom-0 right-0 z-20 h-[300px] w-fit rounded-br-xl md:h-[600px]"
+				/>
+			</div>
+			<h1 className="px-3 text-end text-5xl font-bold uppercase tracking-wide text-brandblue dark:text-foreground md:-translate-y-4 md:text-start md:text-7xl lg:text-9xl">
+				BFM Community
+			</h1>
+		</section>
+	)
 }
