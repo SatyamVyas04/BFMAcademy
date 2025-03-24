f65ac05f2cdb85c5c692ff51d51dbc6057248560
SatyamVyas04 <vyas.satyam.04@gmail.com>
2025-03-24 23:45:55 +0530

    Refactor Section 7 styles for improved layout and readability

--- src/pages/section7.tsx
+++ src/pages/section7.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 
 export default function Section7() {
 	return (
-		<Card className="relative mt-6 min-h-96 w-full overflow-clip rounded-lg bg-zinc-100 text-brandblue transition-all hover:-translate-y-2 hover:shadow-xl md:rounded-xl">
+		<Card className="relative mt-6 min-h-96 w-full overflow-clip rounded-lg bg-zinc-100 pl-3 pt-1 text-brandblue transition-all hover:-translate-y-2 hover:shadow-xl md:rounded-xl">
 			<div className="absolute inset-0 z-10 from-background/20 to-background dark:from-foreground/20 dark:to-foreground max-md:bg-gradient-to-b"></div>
-			<h1 className="relative z-10 max-w-3xl p-4 text-center text-3xl font-bold uppercase md:text-start md:text-5xl lg:text-7xl">
+			<h1 className="relative z-10 max-w-3xl p-4 text-center text-3xl font-bold uppercase md:text-start md:text-5xl lg:text-6xl">
 				INCUBATION & PROJECT LAUNCH SUPPORT
 			</h1>
 			<Image
@@ -15,10 +15,10 @@ export default function Section7() {
 				height={1200}
 				width={1200}
 				alt="Avatars"
-				className="absolute right-0 top-0 z-0 h-full w-fit object-cover max-lg:opacity-20"
+				className="absolute right-0 top-0 z-0 h-full w-fit object-cover mix-blend-multiply max-xl:opacity-20"
 			/>
 			<p className="relative z-10 px-4 py-2 text-lg md:text-start">
-				We don&apos;t just teach Web3 – we help you build in Web3!
+				We don&apos;t just teach Web3 - we help you build in Web3!
 			</p>
 			<ul className="relative z-10 mx-4 my-2 mt-0 space-y-1 pb-6 md:mx-8">
 				<li className="flex max-w-xl items-start justify-start gap-2">
